5d4dcf960cb81e61cc3ef2c6224fdc4a82510d7c
Arghadeep Chakraborty <argho.chakr@gmail.com>
2023-11-27 06:41:30 -0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [weight, setWeight] = useState(null);
+
+  useEffect(() => {
+    // Make API call
+    axios.get('https://scalekit-backend.onrender.com/api/get-weight', {
+      // Add any additional config options if needed
+    })
+      .then(response => {
+        // Assuming the API response has properties 'code', 'data', and 'message'
+        if (response.data.code === 200) {
+          // Check if the response code is 200 (success)
+          setWeight(response.data.data.weight);
+        } else {
+          console.error('Error in API response:', response.data.message);
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); // Empty dependency array means this effect will only run once after the initial render
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {weight !== null ? (
+        <h1>Weight: {weight}</h1>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
